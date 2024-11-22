380967906d0b219bc01933474ee4ea68a74f9cc8
Jhonata-Jackson <jhonata.jacksondev@gmail.com>
2024-11-22 08:08:07 -0300

    feat: :sparkles: set tabbleview to homevc

---
+++ MovieApp/MovieApp/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewTableViewCell.swift
+//  MovieApp
+//
+//  Created by Jhonata Jackson on 22/11/24.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
