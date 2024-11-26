7594ec06d2e67d84b4373754b92bb9a5a5ed3ee6
Jhonata-Jackson <jhonata.jacksondev@gmail.com>
2024-11-25 21:38:15 -0300

    feat: :sparkles: create upcoming screen

--- MovieApp/MovieApp/Controllers/UpcomingVC.swift
+++ MovieApp/MovieApp/Controllers/UpcomingVC.swift
@@ -8,10 +8,62 @@
 import UIKit
 
 class UpcomingVC: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    let upcomingTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        featchUpcoming()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func featchUpcoming() {
+        APICaller.shared.getUpcomingMovies {[weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case.failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension UpcomingVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell
+        let title = titles[indexPath.row]
+        cell?.configure(with:  TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Unknown title", posterURL: title.poster_path ?? ""))
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 }
---
+++ MovieApp/MovieApp/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  MovieApp
+//
+//  Created by Jhonata Jackson on 25/11/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
