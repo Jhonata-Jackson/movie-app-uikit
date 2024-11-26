0d99be3b57bdbbd4cf0083e4ef89f6c4b337ddb8
Jhonata-Jackson <jhonata.jacksondev@gmail.com>
2024-11-26 08:49:26 -0300

    feat: :sparkles: creates the top search screen and implements the search movie on api

---
+++ MovieApp/MovieApp/Controllers/Core/SearchVC.swift
@@ -0,0 +1,108 @@
+//
+//  HomeVC.swift
+//  MovieApp
+//
+//  Created by Jhonata Jackson on 21/11/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    let discoverTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
+    
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultsVC())
+        controller.searchBar.placeholder = "Search for a Movie or a Tv show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(discoverTable)
+        
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.tintColor = .white
+        
+        fetchDiscoverMovies()
+        
+        searchController.searchResultsUpdater = self
+    }
+    
+    private func fetchDiscoverMovies() {
+        APICaller.shared.getDiscoverMovies  {[weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case.failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
+}
+
+extension SearchVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell
+        
+        let model = TitleViewModel(titleName: titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown title", posterURL: titles[indexPath.row].poster_path ?? "")
+        
+        cell?.configure(with: model)
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+}
+
+extension SearchVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count > 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsVC else { return }
+        
+        APICaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultCollectionView.reloadData()
+                case.failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
---
+++ MovieApp/MovieApp/Controllers/General/SearchResultsVC.swift
@@ -0,0 +1,57 @@
+//
+//  SearchResultsVC.swift
+//  MovieApp
+//
+//  Created by Jhonata Jackson on 26/11/24.
+//
+
+import UIKit
+
+class SearchResultsVC: UIViewController {
+    
+    public var titles: [Title] = [Title]()
+    
+    public let searchResultCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10, height: 200)
+        layout.minimumInteritemSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.identifier)
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchResultCollectionView)
+        
+        searchResultCollectionView.delegate = self
+        searchResultCollectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        searchResultCollectionView.frame = view.bounds
+    }
+}
+
+extension SearchResultsVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: title.poster_path ?? "")
+        
+        return cell
+    }
+    
+    
+}
